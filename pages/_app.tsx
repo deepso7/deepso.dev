042132a711631b395edd15fc32339f3117272c21
deepso <deeps.o7o8.y@gmail.com>
2022-04-17 04:51:29 +0530

    default dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>Deepso</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
@@ -41,5 +41,5 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
 }
 
 App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
+  colorScheme: getCookie('mantine-color-scheme', ctx) || 'dark',
 });
