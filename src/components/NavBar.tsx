b689996742554f5f319eae5d6306bf7339ebe3f4
deepso <deeps.o7o8.y@gmail.com>
2022-09-17 02:48:13 +0530

    hide Navbar content on mobile devices

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar: FC = () => {
   return (
     <nav className="relative mx-auto flex w-full max-w-full items-center gap-10 bg-[#14141499] py-8 px-16">
       <h1 className="font-rotterin text-4xl text-custom-sky1">Deepso.dev</h1>
-      <ul className="mt-2 flex gap-6">
+      <ul className="mt-2  hidden gap-6 sm:flex">
         <li className="font-inter text-sm text-custom-sky1">Home</li>
         <li className="font-inter text-sm text-custom-sky1">Blogs</li>
         <li className="font-inter text-sm text-custom-sky1">Snippets</li>
