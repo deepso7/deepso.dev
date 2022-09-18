e33b6bb6a3c9dca6db37614d30807490bf2a96ce
deepso <deeps.o7o8.y@gmail.com>
2022-09-19 04:35:04 +0530

    add hero text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,18 @@ import NavBar from "../components/NavBar";
 const Home: NextPage = () => (
   <div className="">
     <NavBar />
-    <div className="mx-auto max-w-7xl">
-      <h1>HI</h1>
-      <p>Sed</p>
+    <div className="mx-auto max-w-4xl px-6 md:py-14">
+      <h1 className="bg-gradient-to-r from-[#FFDBFB] via-[#B28DFF] to-[#AEC0FF] bg-clip-text font-rotterin text-4xl text-transparent md:text-7xl">
+        Deepanshu Singh
+      </h1>
+      <h2 className="py-3 text-sm md:px-2 md:text-xl">
+        <span className="text-[#D1D2D3]">Lead Developer at </span>
+        <span className="font-extrabold text-[#E6E6E6]">
+          <a href="https://huddle01.com/" target="_blank">
+            Huddle01
+          </a>
+        </span>
+      </h2>
     </div>
   </div>
 );
