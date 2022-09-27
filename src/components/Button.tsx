0f1181ee585b335534fbed1d70a4e78f843c8685
deepso <deeps.o7o8.y@gmail.com>
2022-09-28 02:24:22 +0530

    responsive category and view component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,8 +14,8 @@ const Arrow = () => (
 );
 
 const Button: React.FC<{ text: string }> = ({ text }) => (
-  <button className="pr- flex h-14 w-fit items-center rounded-2xl border-2 border-[#374151] px-8 text-[#929497] hover:bg-[#2D3747]">
-    {text}&nbsp;
+  <button className="flex h-14 w-fit items-center gap-5 rounded-2xl border-2 border-[#374151] px-8 text-[#929497] hover:bg-[#2D3747]">
+    {text}
     <Arrow />
   </button>
 );
--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -21,7 +21,7 @@ const Tab: React.FC<{
   Icon: JSX.Element;
   text: string;
 }> = ({ Icon, text }) => (
-  <div className="flex items-center gap-2">
+  <div className="flex items-center md:gap-2">
     {Icon}
     <span className="text-[#A7A9AC]">{text}</span>
   </div>
@@ -29,7 +29,10 @@ const Tab: React.FC<{
 
 const FeatureCard: React.FC<{ gradient: string }> = ({ gradient }) => {
   return (
-    <GradientContainer gradient={gradient} padding="px-8 pt-6 pb-8">
+    <GradientContainer
+      gradient={gradient}
+      padding="md:px-8 md:pt-6 md:pb-8 px-3 pt-4 pb-4"
+    >
       <h3 className="text-2xl font-semibold text-[#E6E6E6]">Title</h3>
       <p className="text-xl text-[#D1D2D3]">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ornare
