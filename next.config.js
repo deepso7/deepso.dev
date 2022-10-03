c1eaa92f5fea463e7882dc4257c327caf244fc51
deepso <deeps.o7o8.y@gmail.com>
2022-10-03 18:49:52 +0530

    add splitbee

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ module.exports = {
   images: {
     domains: ["i.scdn.co"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import splitbee from "@splitbee/web";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect((): void => {
+    splitbee.init({
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    });
+  }, []);
+
   return (
     <div className="min-h-screen bg-custom-black">
       <Component {...pageProps} />
