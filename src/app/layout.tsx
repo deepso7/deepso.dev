87155ca190226c76456894b325f99ce2e99f0edc
Deepso <deeps.o7o8.y@gmail.com>
2022-12-07 18:45:02 +0530

    add analytics and bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,13 @@
 import "../styles/globals.css";
+import { Inter as FontSans } from "@next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Analytics } from "@/components/Analytics";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
@@ -6,13 +15,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={cn(
+        "bg-zinc-800 font-sans text-white antialiased",
+        fontSans.variable
+      )}
+    >
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
