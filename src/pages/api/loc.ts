4bfa1d56e30b3b7a583756ac46160e89a69f231e
deepso <deeps.o7o8.y@gmail.com>
2023-04-04 12:28:25 +0530

    update edge func

--- src/pages/api/loc.ts
+++ src/pages/api/loc.ts
@@ -1,14 +1,14 @@
-import { geolocation } from '@vercel/edge';
+import { geolocation } from "@vercel/edge";
 
 export const config = {
-  runtime: 'edge',
+  runtime: "edge",
 };
 
 export default function (request: Request) {
-  const { city } = geolocation(request);
+  const geo = geolocation(request);
   // You can also get the city using dot notation on the function
   // const city = geolocation(request).city;
-  return new Response(`<h1>Your location is ${city}</h1>`, {
-    headers: { 'content-type': 'text/html' },
+  return new Response(JSON.stringify(geo), {
+    headers: { "content-type": "application/json" },
   });
 }
