511373a7c014004332688b909e686041dc244bc1
deepso <deeps.o7o8.y@gmail.com>
2023-04-05 11:03:09 +0530

    change fetch location logic

--- src/pages/api/loc.ts
+++ src/pages/api/loc.ts
@@ -2,18 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
   runtime: "edge", // this is a pre-requisite
-  regions: ["bom1", "iad1"], // only execute this function on iad1
 };
 
-export default async (req: NextRequest) => {
-  const data = await fetch("https://deepso.dev/api/loc");
-
-  const response = await data.json();
-
-  console.log({ response });
-
+export default (req: NextRequest) => {
   return NextResponse.json({
     geo: req.geo,
-    response,
   });
 };
---
+++ src/pages/api/test.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  runtime: "edge", // this is a pre-requisite
+  regions: ["bom1", "iad1"], // only execute this function on iad1
+};
+
+export default async (req: NextRequest) => {
+  const data = await fetch("https://deepso.dev/api/loc");
+
+  const response = await data.json();
+
+  return NextResponse.json({
+    response,
+  });
+};
