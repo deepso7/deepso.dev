27911da09677bc7a81a551523081e3f6d7320e3d
deepso <deeps.o7o8.y@gmail.com>
2025-04-25 18:13:48 +0530

    add dark mode toggle

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,35 @@
+import { Moon, Sun } from 'lucide-react';
+import * as React from 'react';
+
+import { Button } from '@/components/ui/button';
+
+export function ModeToggle() {
+  const [theme, setThemeState] = React.useState<
+    'theme-light' | 'dark' | 'system'
+  >('theme-light');
+
+  React.useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains('dark');
+    setThemeState(isDarkMode ? 'dark' : 'theme-light');
+  }, []);
+
+  React.useEffect(() => {
+    const isDark =
+      theme === 'dark' ||
+      (theme === 'system' &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+    document.documentElement.classList[isDark ? 'add' : 'remove']('dark');
+  }, [theme]);
+
+  return (
+    <Button
+      variant="secondary"
+      size="icon"
+      onClick={() => setThemeState(theme === 'dark' ? 'theme-light' : 'dark')}
+    >
+      <Sun className="dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
