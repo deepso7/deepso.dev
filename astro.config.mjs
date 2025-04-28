f196096ac84f29b79d86ba2534829ede97d8601d
deepso <deeps.o7o8.y@gmail.com>
2025-04-29 00:12:00 +0530

    fix layout shift

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   experimental: {
+    responsiveImages: true,
     fonts: [
       {
         provider: fontProviders.google(),
