12ddf3450fb4f6d8f7eae764b87ca576d03fc7c5
deepso <deeps.o7o8.y@gmail.com>
2025-04-28 23:25:02 +0530

    add animated icon for dark and light mode

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,7 +1,8 @@
-import { Moon, Sun } from 'lucide-react';
 import * as React from 'react';
 
 import { Button } from '@/components/ui/button';
+import { MoonIcon } from '@/components/ui/moon';
+import { SunIcon } from '@/components/ui/sun';
 
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<
@@ -27,8 +28,14 @@ export function ModeToggle() {
       size="icon"
       onClick={() => setThemeState(theme === 'dark' ? 'theme-light' : 'dark')}
     >
-      <Sun className="dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <SunIcon
+        className="dark:-rotate-90 rotate-0 scale-100 transition-all dark:scale-0"
+        size={20}
+      />
+      <MoonIcon
+        className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        size={20}
+      />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
