0d55bf8623e8448c172504f1c3e2798fb3c3037d
Deepso <deeps.o7o8.y@gmail.com>
2026-01-21 18:12:39 +0530

    update icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,37 +9,49 @@ export const Footer = () => {
     <footer className="flex items-center justify-between pt-8">
       <ul className="horizontal space-x-4">
         <li>
-          <Button size="icon-lg" variant="secondary">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/deepso7"
-            >
-              <GithubIcon className="flex cursor-pointer items-center justify-center rounded-md p-2 transition-colors duration-200 select-none hover:bg-accent" />
-            </a>
-          </Button>
+          <Button
+            size="icon-lg"
+            variant="secondary"
+            render={
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/deepso7"
+              >
+                <GithubIcon className="flex cursor-pointer items-center justify-center rounded-md p-2 transition-colors duration-200 select-none hover:bg-accent" />
+              </a>
+            }
+          />
         </li>
         <li>
-          <Button size="icon-lg" variant="secondary">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://twitter.com/deepso7"
-            >
-              <TwitterIcon className="flex cursor-pointer items-center justify-center rounded-md p-2 transition-colors duration-200 select-none hover:bg-accent" />
-            </a>
-          </Button>
+          <Button
+            size="icon-lg"
+            variant="secondary"
+            render={
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://twitter.com/deepso7"
+              >
+                <TwitterIcon className="flex cursor-pointer items-center justify-center rounded-md p-2 transition-colors duration-200 select-none hover:bg-accent" />
+              </a>
+            }
+          />
         </li>
         <li>
-          <Button size="icon-lg" variant="secondary">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/deepso7"
-            >
-              <LinkedinIcon className="flex cursor-pointer items-center justify-center rounded-md p-2 transition-colors duration-200 select-none hover:bg-accent" />
-            </a>
-          </Button>
+          <Button
+            size="icon-lg"
+            variant="secondary"
+            render={
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/deepso7"
+              >
+                <LinkedinIcon className="flex cursor-pointer items-center justify-center rounded-md p-2 transition-colors duration-200 select-none hover:bg-accent" />
+              </a>
+            }
+          />
         </li>
       </ul>
 
